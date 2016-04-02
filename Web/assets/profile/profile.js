bd481a61f6f5c78cde497bb1fab1bcf98d88aa97
cody <cody.mikol@boltstech.com>
2016-04-02 15:29:05 -0400

    Added semi profile page

--- Web/assets/profile/profile.js
+++ Web/assets/profile/profile.js
@@ -16,6 +16,18 @@
 
         console.log("Controller Loaded [profile.js]");
 
+        UserService.getData('assets/profile/profile.json').then(
+            function(resolve){
+                //success
+                vm.profile = resolve.profile[0];
+                console.log("loaded profile picture");
+            },
+            function(reject){
+                //failure
+                console.log("Failed to load tile icons :(");
+            }
+        );
+
 
         //vm.animateState = function(id, cssID, newClass, timeout) {
         //    UserService.animateState(id, cssID, newClass, timeout);
